63f9b9ffdeff53f86033a50a229c666974d4b9b1
JimboMachine <xbox00210@gmail.com>
2023-09-12 22:16:14 +0800

    Add some discreption

---
+++ Cplusplus/src/hw1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    /*
+    Please write some code here.
+    If you find some code can be reused, please be done in hw.h
+    ,and includ hw.h that is/are your function(s) before you run testcases.
+
+    I hope you can do it by your own.
+    Please feel free to search syntax if you totally forget.
+
+    The only rule is "Do your best."
+    Your sanpai Jimbo.
+    Best regards.
+    */
+
+   //Start Here
+
+    return 0;
+}
